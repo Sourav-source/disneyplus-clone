5a4e25300db82d5a6c18fb2f9079ecbd5cb4512a
Sourav-source <st.sourav.xaverian@gmail.com>
2021-11-21 09:58:19 +0530

    ADD: detail page

--- src/components/Detail.js
+++ src/components/Detail.js
@@ -12,20 +12,26 @@ function Detail() {
       </ImageTitle>
       <Controls>
         <PlayButton>
-        <img src = "/images/play-icon-black.png" />
-        <span>PLAY</span>
+          <img src="/images/play-icon-black.png" />
+          <span>PLAY</span>
         </PlayButton>
         <TrailerButton>
-        <img src = "/images/play-icon-white.png" />
-        <span>TRAILER</span>
+          <img src="/images/play-icon-white.png" />
+          <span>TRAILER</span>
         </TrailerButton>
         <AddButton>
-
+          <span>+</span>
         </AddButton>
         <GroupWatchButton>
-
+          <img src="/images/group-icon.png" />
         </GroupWatchButton>
       </Controls>
+      <SubTitle>2018 . 7m . Family, Fantasy, Kids, Animation</SubTitle>
+      <Description>
+        A Chinese mom who's sad when her grown son leaves gets another chance at
+        motherhood when one of her dumplings springs to life.But she finds that
+        nothing stays cute and small forever.
+      </Description>
     </Container>
   );
 }
@@ -64,10 +70,58 @@ const ImageTitle = styled.div`
     object-fit: contain;
   }
 `;
-const Controls = styled.div``
+const Controls = styled.div`
+  display: flex;
+  align-items: center;
+`;
 const PlayButton = styled.button`
-  
-`
-const TrailerButton = styled.button``
-const AddButton = styled.button``
-const GroupWatchButton = styled.button``
+  border-radius: 4px;
+  font-size: 15px;
+  padding: 0 24px;
+  margin-right: 22px;
+  display: flex;
+  align-items: center;
+  height: 56px;
+  background: rgb (249, 249, 249);
+  border: none;
+  letter-spacing: 1.8px;
+  cursor: pointer;
+`;
+const TrailerButton = styled(PlayButton)`
+  background: rgba(0, 0, 0, 0.3);
+  border: 1px solid rgb(249, 249, 249);
+  color: rgb(249, 249, 249);
+  text-transform: uppercase;
+`;
+const AddButton = styled.button`
+  margin-right: 16px;
+  width: 44px;
+  height: 44px;
+  display: flex;
+  align-items: center;
+  justify-content: center;
+  border-radius: 50%;
+  border: 2px solid white;
+  background-color: rgba(0, 0, 0, 0.6);
+  cursor: pointer;
+
+  span {
+    font-size: 30px;
+    color: white;
+  }
+`;
+const GroupWatchButton = styled(AddButton)`
+  background: rgb(0, 0, 0);
+`;
+const SubTitle = styled.div`
+  color: rgb(249, 249, 249);
+  font-size: 15px;
+  min-height: 20px;
+  margin-top: 26px;
+`;
+const Description = styled.div`
+  line-height: 1.4;
+  font-size: 20px;
+  margin-top: 16px;
+  color: rgb(249, 249, 249);
+`;
